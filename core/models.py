606a70e431242f9944be6fd891a8281bed6c8f12
coffeau <eduarda6saibert@gmail.com>
2022-06-29 15:01:48 -0300

    Model comentários

--- core/models.py
+++ core/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+# Módulos
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.nome
+        return self.nome.upper()
 
 
 class Categoria(models.Model):
